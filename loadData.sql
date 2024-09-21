c970c7ebffd1f797b3d4c140b26184881c14aa65
iyasin <iayasin771@gmail.com>
2024-09-21 18:39:53 -0400

    Do Distinct

--- loadData.sql
+++ loadData.sql
@@ -10,7 +10,7 @@
 
 -- INSERT INTO Users from Project1
 INSERT INTO Users (user_id, first_name, last_name, year_of_birth, month_of_birth, day_of_birth, gender)
-SELECT user_id, first_name, last_name, year_of_birth, month_of_birth, day_of_birth, gender
+SELECT DISTINCT user_id, first_name, last_name, year_of_birth, month_of_birth, day_of_birth, gender
 FROM project1.Public_User_Information;
 
 -- CREATE TABLE Friends 
