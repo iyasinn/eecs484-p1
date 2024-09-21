7f09607aa6c161bee3cedda0fa890cf31abfd475
yalhanuma <alhanuma@umich.edu>
2024-09-21 14:42:58 -0400

    added friends trigger/testing git

--- createTables.sql
+++ createTables.sql
@@ -129,3 +129,16 @@ CREATE TABLE Tags (
     FOREIGN KEY (tag_photo_id) REFERENCES Photos(photo_id),
     FOREIGN KEY (tag_subject_id) REFERENCES Users(user_id)
 );
+
+CREATE TRIGGER Order_Friend_Pairs
+    BEFORE INSERT ON Friends
+    FOR EACH ROW
+        DECLARE temp INTEGER;
+        BEGIN
+            IF :NEW.user1_id > :NEW.user2_id THEN
+                temp := :NEW.user2_id;
+                :NEW.user2_id := :NEW.user1_id;
+                :NEW.user1_id := temp;
+            END IF;
+        END;
+/
