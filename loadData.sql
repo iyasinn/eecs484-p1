1c77a2116ed5870bd8df2f229dce3ca673842c30
iayasin771@gmail.com <iayasin771@gmail.com>
2024-09-21 18:29:48 -0400

    Done

---
+++ loadData.sql
@@ -0,0 +1,38 @@
+-- CREATE TABLE Users 
+-- -
+-- user_id
+-- first_name
+-- last_name
+-- year_of_birth
+-- month_of_birth
+-- day_of_birth
+-- gender
+
+-- INSERT INTO Users from Project1
+INSERT INTO Users (user_id, first_name, last_name, year_of_birth, month_of_birth, day_of_birth, gender)
+SELECT user_id, first_name, last_name, year_of_birth, month_of_birth, day_of_birth, gender
+FROM Project1;
+
+-- CREATE TABLE Friends 
+
+-- CREATE TABLE Cities
+
+-- CREATE TABLE User_Current_Cities 
+
+-- CREATE TABLE User_Hometown_Cities 
+
+-- CREATE TABLE Messages 
+
+-- CREATE TABLE Programs
+
+-- CREATE TABLE Education 
+
+-- CREATE TABLE User_Events 
+
+-- CREATE TABLE Participants 
+
+-- CREATE TABLE Albums 
+
+-- CREATE TABLE Photos
+
+-- CREATE TABLE Tags
